a4aca0426ddaf747784cb7989e425dff25eaa0e9
Antti Leppä <antti.leppa@metatavu.fi>
2018-03-15 12:54:32 +0200

    Update pakkasmarja-management.php

--- pakkasmarja-management.php
+++ pakkasmarja-management.php
@@ -3,7 +3,7 @@
  * Created on Jun 21, 2017
  * Plugin Name: Pakkasmarja Management
  * Description: Management functions for Pakkasmarja Application
- * Version: 0.0.1
+ * Version: 0.1.1
  * Author: Metatavu Oy
  */
 
